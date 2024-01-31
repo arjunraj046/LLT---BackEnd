4f5ecf369f7d743100e583917e6397fb218b8a0f
NEXDev1 <152363696+NEXDev1@users.noreply.github.com>
2024-01-31 11:58:41 +0530

    agent complete

--- database/repository/agentRepository.js
+++ database/repository/agentRepository.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Types;
-
-// Assuming you have defined the UserData model somewhere before using it in addagentDataDB function
-// const UserData = mongoose.model('UserData');
-// const UserData = require("../models/TokenSchema");
 const Order = require("../models/OrderSchema");
 const Token = require("../models/TokenSchema");
 
@@ -32,7 +28,6 @@ const addAgentDataDB = async (userId, drawTime, date, tokenList) => {
           return await newToken.save();
         });
 
-        // Wait for all token saves to complete
         return Promise.all(tokenPromises);
       })
       .catch((error) => {
@@ -46,56 +41,40 @@ const addAgentDataDB = async (userId, drawTime, date, tokenList) => {
   }
 };
 
-// const UserData = require("../models/UserData");
-// // const { ObjectId } = require("mongoose").Types;
-
-// const addagentDataDB = async (id, date, tokenNumber, count) => {
-//   try {
-//     const UserData = mongoose.model('UserData');
-
-//     const userData = new UserData({
-//       userId: ObjectId(id),
-//       tokenNumber: tokenNumber,
-//       count: count,
-//       date: date,
-//     });
-
-//     const savedUserData = await userData.save();
-//     return savedUserData;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// const getAgentEntity = async (id) => {
-//   try {
-//     console.log("getAgentEntity in db ", id);
-//     let _id = new mongoose.Types.ObjectId(id);
-//     console.log(_id);
-//     const list = await UserData.find({ userId: _id });
-//     console.log(list);
-//     if (!list) return null;
-//     return list;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 const getAgentEntity = async (id) => {
   try {
     console.log("getAgentEntity in db ", id);
     const _id = new mongoose.Types.ObjectId(id);
     console.log(_id);
 
-    const list = await UserData.aggregate([
+    const list = await Order.aggregate([
       {
-        $match: { userId: _id },
+        $match: {
+          userId: new ObjectId(id),
+        },
       },
       {
         $lookup: {
-          from: "users",
-          localField: "userId",
-          foreignField: "_id",
-          as: "result",
+          from: "tokens",
+          localField: "_id",
+          foreignField: "orderId",
+          as: "token",
+        },
+      },
+      {
+        $unwind: {
+          path: "$token",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          userId: 1,
+          date: 1,
+          drawTime: 1,
+          tokenId: "$token._id",
+          tokenNumber: "$token.tokenNumber",
+          tokenCount: "$token.count",
         },
       },
     ]);
@@ -104,10 +83,8 @@ const getAgentEntity = async (id) => {
       return null;
     }
 
-    // Check if the result array has elements and extract the username
     const username = list[0].result[0].userName;
 
-    // Flatten the structure and include userName in each document
     const modifiedList = list.map((item) => {
       const { result, ...rest } = item;
       return { ...rest, userName: username };
@@ -117,17 +94,41 @@ const getAgentEntity = async (id) => {
     return modifiedList;
   } catch (error) {
     console.error("Error fetching agent entities:", error);
-    throw error; // Re-throw the error to be handled by the caller
+    throw error;
   }
 };
 
-const deleteEntity = async (id) => {
+const getAgentOrders = async (id) => {
   try {
-    console.log("deleteAgentEntity in db ", id);
     const _id = new mongoose.Types.ObjectId(id);
     console.log(_id);
-    const deleteItem = await UserData.deleteOne({ _id });
-    return deleteItem;
+
+    const list = await Order.aggregate([
+      {
+        $match: {
+          userId: new ObjectId(id),
+        },
+      },
+      {
+        $lookup: {
+          from: "tokens",
+          localField: "_id",
+          foreignField: "orderId",
+          as: "token",
+        },
+      },
+    ]);
+  } catch (error) {}
+};
+
+const deleteEntity = async (orderId) => {
+  try {
+    console.log("deleteAgentOrder in db ", orderId);
+    const _id = new mongoose.Types.ObjectId(orderId);
+    console.log(_id);
+    const deleteTokens = await Token.deleteMany({ orderId: _id });
+    const deleteOrder = await Order.deleteOne({ _id });
+    return { deleteTokens, deleteOrder };
   } catch (error) {
     console.error("Error fetching agent entities:", error);
     throw error;
